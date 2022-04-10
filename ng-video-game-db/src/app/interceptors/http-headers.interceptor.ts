4f4512198380d7ff2790a2c032344620f1c085fe
rotkle <lediepnguyenvan@gmail.com>
2022-04-10 17:00:38 +0700

    implement get list and sort data from APIs

--- ng-video-game-db/src/app/interceptors/http-headers.interceptor.ts
+++ ng-video-game-db/src/app/interceptors/http-headers.interceptor.ts
@@ -9,8 +9,13 @@ export class HttpHeadersInterceptor implements HttpInterceptor{
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         req = req.clone({
             setHeaders: {
-                
+                'x-rapidapi-key': '2216dfeb63msh2a7fef0a577838ep18d1a7jsn9951b0fa8141',
+                'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
             },
-        })
+            setParams:{
+                key: 'e877263328994ca1adc8d9b1bea51d61',
+            }
+        });
+        return next.handle(req);
     }
 }
